c0cc48f7ea86af8dc979e563f64c8800b12cd827
PESolut <jahaadpetty@pursuit.org>
2022-08-01 12:43:01 -0400

    03-filter.js NPM test passed

--- src/03-filter.js
+++ src/03-filter.js
@@ -13,21 +13,45 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {}
+function getSongsBySaib(songs) {
+  if (songs.length === 0){
+    throw 'error, songs array needs to be more than 0 in length'
+  }
+  const reqSongs = songs.filter(song => {
+    return song.artist === 'Saib'
+  })
+  return reqSongs
+}
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsOverThreeMinutes(songs) {}
+function getSongsOverThreeMinutes(songs) {
+  if (songs.length === 0){
+    throw 'error, songs array needs to be more than 0 in length'
+  }
+  const overThree = songs.filter(song => {
+    return song.runtimeInSeconds > 180
+  })
+  return overThree
+}
 
 /**
  * Returns an array of songs where the song title is the same as the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getTitleTracks(songs) {}
+function getTitleTracks(songs) {
+  if (songs.length === 0){
+    throw 'error, songs array needs to be more than 0'
+  }
+  const sameTitleAndAlbum = songs.filter(song => {
+    return song.title === song.album
+  })
+  return sameTitleAndAlbum
+}
 
 module.exports = {
   getSongsBySaib,
